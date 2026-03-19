a63e8398502d8db19c4057a2fa94b7daf4dd667b
Bruno Lima <bruuno.lima03@gmail.com>
2026-03-19 11:40:05 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,56 @@
 function formulario() {
-	const selecionar = document.querySelector(".formulario_selecionar")
+	const selecionar = document.querySelector(".selecionar")
 	const adicionar = document.querySelector(".botao_adicionar")
-	const container = document.querySelector(".inputs_container")
+	const containerInputs = document.querySelector(".inputs")
 	adicionar.addEventListener("click", () => {
-		const inputs = document.querySelectorAll(".input_" + selecionar.value)
-		inputs.forEach(input => {
-			if (input.classList.contains("oculto")) {
-				input.classList.remove("oculto")
-				container.appendChild(input)
-			}
-		})
-	})
-	const botoesRemover = document.querySelectorAll(".botao_remover")
-	botoesRemover.forEach(remover => {
-		remover.addEventListener("click", () => {
-			remover.parentElement.classList.add("oculto")
-		})
+		if (selecionar.value === "email") {
+			const campoSelecionado = document.createElement("div")
+			campoSelecionado.classList.add("campo_selecionado")
+			const campo = document.createElement("input")
+			campo.type = "text"
+			campo.classList.add("form-control")
+			campo.placeholder="Email"
+			const remover = document.createElement("button")
+			remover.classList.add("btn",  "btn-danger", "botao_remover")
+			remover.textContent = "X"
+			campoSelecionado.appendChild(campo)
+			campoSelecionado.appendChild(remover)
+			containerInputs.appendChild(campoSelecionado)
+			remover.addEventListener("click", () => {
+				campoSelecionado.classList.add("oculto")
+			})
+		} else if (selecionar.value === "celular") {
+			const campoSelecionado = document.createElement("div")
+			campoSelecionado.classList.add("campo_selecionado")
+			const campo = document.createElement("input")
+			campo.type = "text"
+			campo.classList.add("form-control")
+			campo.placeholder="Celular"
+			const remover = document.createElement("button")
+			remover.classList.add("btn",  "btn-danger", "botao_remover")
+			remover.textContent = "X"
+			campoSelecionado.appendChild(campo)
+			campoSelecionado.appendChild(remover)
+			containerInputs.appendChild(campoSelecionado)
+			remover.addEventListener("click", () => {
+				campoSelecionado.classList.add("oculto")
+			})
+		} else if (selecionar.value === "endereco") {
+			const campoSelecionado = document.createElement("div")
+			campoSelecionado.classList.add("campo_selecionado")
+			const campo = document.createElement("textarea")
+			campo.classList.add("form-control")
+			campo.placeholder="Endereço"
+			const remover = document.createElement("button")
+			remover.classList.add("btn",  "btn-danger", "botao_remover")
+			remover.textContent = "X"
+			campoSelecionado.appendChild(campo)
+			campoSelecionado.appendChild(remover)
+			containerInputs.appendChild(campoSelecionado)
+			remover.addEventListener("click", () => {
+				campoSelecionado.classList.add("oculto")
+			})
+		}
 	})
 }
 formulario()
