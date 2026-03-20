c27104ca0f82fd4640a10d57e2868160dc4b5d99
Bruno Lima <bruuno.lima03@gmail.com>
2026-03-20 10:52:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,33 @@
-function formulario() {
-	const selecionar = document.querySelector(".selecionar")
-	const adicionar = document.querySelector(".adicionar")
-	const campos = document.querySelector(".campos")
-	adicionar.addEventListener("click", () => {
-		if (selecionar.value) {
-			const campo = document.createElement("div")
-			campo.classList.add("campo")
-			let campoInput
-			if (selecionar.value === "endereco") {
-				campoInput = document.createElement("textarea")
-			} else {
-				campoInput = document.createElement("input")
-				campoInput.type = "text"
-			}
+const adicionar = document.querySelector(".adicionar")
+const campos = document.querySelector(".campos")
+const selecionar = document.querySelector(".selecionar")
+adicionar.addEventListener("click", () => {
+	if (selecionar.value) {
+		const campo = document.createElement("div")
+		campo.classList.add("campo")
+		if (selecionar.value === "endereco") {
+			const campoTextarea = document.createElement("textarea")
+			campoTextarea.classList.add("form-control")
+			campoTextarea.placeholder = "Endereço"
+			campo.appendChild(campoTextarea)
+		} else {
+			const campoInput = document.createElement("input")
+			campoInput.type = "text"
 			campoInput.classList.add("form-control")
-			const placeholders = {
-				email: "Email",
-				celular: "Celular",
-				endereco: "Endereço"
-			}
-			campoInput.placeholder = placeholders[selecionar.value]
-			const remover = document.createElement("button")
-			remover.classList.add("btn", "btn-danger", "remover")
-			remover.textContent = "X"
 			campo.appendChild(campoInput)
-			campo.appendChild(remover)
-			remover.addEventListener("click", () => {
-				campo.remove()
-			})
-			campos.appendChild(campo)
+			if (selecionar.value === "email") {
+				campoInput.placeholder = "Email"
+			} else {
+				campoInput.placeholder = "Celular"
+			}
 		}
-	})
-}
-formulario()
+		const remover = document.createElement("button")
+		remover.classList.add("btn", "btn-danger", "remover")
+		remover.textContent = "X"
+		campo.appendChild(remover)
+		campos.appendChild(campo)
+		remover.addEventListener("click", () => {
+			campo.remove()
+		})
+	}
+})
