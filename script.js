27d253c4118d16c55cae706995a7c77d8addbe2b
Bruno Lima <bruuno.lima03@gmail.com>
2026-03-20 10:22:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,54 +3,32 @@ function formulario() {
 	const adicionar = document.querySelector(".adicionar")
 	const campos = document.querySelector(".campos")
 	adicionar.addEventListener("click", () => {
-		const campoSelecionado = selecionar.value
-		if (campoSelecionado === "email") {
+		if (selecionar.value) {
 			const campo = document.createElement("div")
 			campo.classList.add("campo")
-			const campoInput = document.createElement("input")
-			campoInput.type = "text"
+			let campoInput
+			if (selecionar.value === "endereco") {
+				campoInput = document.createElement("textarea")
+			} else {
+				campoInput = document.createElement("input")
+				campoInput.type = "text"
+			}
 			campoInput.classList.add("form-control")
-			campoInput.placeholder = "Email"
+			const placeholders = {
+				email: "Email",
+				celular: "Celular",
+				endereco: "Endereço"
+			}
+			campoInput.placeholder = placeholders[selecionar.value]
 			const remover = document.createElement("button")
 			remover.classList.add("btn", "btn-danger", "remover")
 			remover.textContent = "X"
 			campo.appendChild(campoInput)
 			campo.appendChild(remover)
-			campos.appendChild(campo)
 			remover.addEventListener("click", () => {
 				campo.remove()
 			})
-		} else if (campoSelecionado === "celular") {
-			const campo = document.createElement("div")
-			campo.classList.add("campo")
-			const campoInput = document.createElement("input")
-			campoInput.type = "text"
-			campoInput.classList.add("form-control")
-			campoInput.placeholder = "Celular"
-			const remover = document.createElement("button")
-			remover.classList.add("btn", "btn-danger", "remover")
-			remover.textContent = "X"
-			campo.appendChild(campoInput)
-			campo.appendChild(remover)
 			campos.appendChild(campo)
-			remover.addEventListener("click", () => {
-				campo.remove()
-			})			
-		} else if (campoSelecionado === "endereco") {
-			const campo = document.createElement("div")
-			campo.classList.add("campo")
-			const campoTextarea = document.createElement("textarea")
-			campoTextarea.classList.add("form-control")
-			campoTextarea.placeholder = "Endereço"
-			const remover = document.createElement("button")
-			remover.classList.add("btn", "btn-danger", "remover")
-			remover.textContent = "X"
-			campo.appendChild(campoTextarea)
-			campo.appendChild(remover)
-			campos.appendChild(campo)	
-			remover.addEventListener("click", () => {
-				campo.remove()
-			})		
 		}
 	})
 }
