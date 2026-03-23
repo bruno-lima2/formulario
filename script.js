e12c689f9d7ef7614298ffb73b713dad6d95b184
Bruno Lima <bruuno.lima03@gmail.com>
2026-03-23 10:23:33 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,39 +1,44 @@
-const selecionar = document.querySelector(".selecionar")
 const adicionar = document.querySelector(".adicionar")
+const selecionar = document.querySelector(".selecionar")
 const campos = document.querySelector(".campos")
 adicionar.addEventListener("click", () => {
 	if (selecionar.value) {
-		const campo = document.createElement("div")
-		campo.classList.add("campo")
-		campos.appendChild(campo)
-		if (selecionar.value === "endereco") {
-			const campoTextarea = document.createElement("textarea")
-			campoTextarea.placeholder = "Endereço"
-			campoTextarea.classList.add("form-control")
-			campo.appendChild(campoTextarea)
-		} else {
-			const campoInput = document.createElement("input")
-			campoInput.classList.add("form-control")
-			campoInput.type = "text"
-			if (selecionar.value === "email") {
-				campoInput.placeholder = "Email"
+		function criarCampo() {
+			const campo = document.createElement("div")
+			campo.classList.add("campo")
+			if (selecionar.value === "endereco") {
+				const campoTextarea = document.createElement("textarea")
+				campoTextarea.classList.add("form-control")
+				campoTextarea.placeholder = "Endereço"
+				campo.appendChild(campoTextarea)
 			} else {
-				campoInput.placeholder = "Celular"
-				campoInput.addEventListener("blur", () => {
-					const valor = campoInput.value.replace(/\D/g, "")
-					if (valor.length !== 11) {
-						campoInput.value = ""
-					}
-				})
+				const campoInput = document.createElement("input")
+				campoInput.type = "text"
+				campoInput.classList.add("form-control")
+				if (selecionar.value === "email") {
+					campoInput.placeholder = "Email"
+				} else {
+					campoInput.placeholder = "Celular"
+					campoInput.addEventListener("blur", () => {
+						if (campoInput.value.replace(/\D/g, "").length !== 11) {
+							campoInput.value = ""
+						}
+					})
+				}
+				campo.appendChild(campoInput)
 			}
-			campo.appendChild(campoInput)
+			campos.appendChild(campo)
+			function botaoRemover() {
+				const remover = document.createElement("button")
+				remover.textContent = "X"
+				remover.classList.add("btn", "btn-danger", "remover")
+				remover.addEventListener("click", () => {
+					campo.remove()
+				})
+				campo.appendChild(remover)
+			}
+			botaoRemover()
 		}
-		const remover = document.createElement("button")
-		remover.classList.add("btn", "btn-danger", "remover")
-		remover.textContent = "X"
-		campo.appendChild(remover)
-		remover.addEventListener("click", () => {
-			campo.remove()
-		})
+		criarCampo()
 	}
 })
