5133e5b5de88b7115cbe66ae77ab34d868f1d70d
Bruno Lima <bruuno.lima03@gmail.com>
2026-03-23 09:04:10 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const selecionar = document.querySelector(".selecionar");
+const selecionar = document.querySelector(".selecionar")
 const adicionar = document.querySelector(".adicionar")
 const campos = document.querySelector(".campos")
 adicionar.addEventListener("click", () => {
@@ -8,19 +8,20 @@ adicionar.addEventListener("click", () => {
 		campos.appendChild(campo)
 		if (selecionar.value === "endereco") {
 			const campoTextarea = document.createElement("textarea")
-			campoTextarea.placeholder="Endereço"
+			campoTextarea.placeholder = "Endereço"
 			campoTextarea.classList.add("form-control")
 			campo.appendChild(campoTextarea)
 		} else {
 			const campoInput = document.createElement("input")
-			campoInput.type="text"
 			campoInput.classList.add("form-control")
+			campoInput.type = "text"
 			if (selecionar.value === "email") {
-				campoInput.placeholder="Email"
+				campoInput.placeholder = "Email"
 			} else {
-				campoInput.placeholder="Celular"
+				campoInput.placeholder = "Celular"
 				campoInput.addEventListener("blur", () => {
-					if (campoInput.value.length !== 11) {
+					const valor = campoInput.value.replace(/\D/g, "")
+					if (valor.length !== 11) {
 						campoInput.value = ""
 					}
 				})
@@ -36,43 +37,3 @@ adicionar.addEventListener("click", () => {
 		})
 	}
 })
-
-/*const adicionar = document.querySelector(".adicionar")
-const campos = document.querySelector(".campos")
-const selecionar = document.querySelector(".selecionar")
-adicionar.addEventListener("click", () => {
-	if (selecionar.value) {
-		const campo = document.createElement("div")
-		campo.classList.add("campo")
-		if (selecionar.value === "endereco") {
-			const campoTextarea = document.createElement("textarea")
-			campoTextarea.classList.add("form-control")
-			campoTextarea.placeholder = "Endereço"
-			campo.appendChild(campoTextarea)
-		} else {
-			const campoInput = document.createElement("input")
-			campoInput.type = "text"
-			campoInput.classList.add("form-control")
-			campo.appendChild(campoInput)
-			if (selecionar.value === "email") {
-				campoInput.placeholder = "Email"
-			} else {
-				campoInput.placeholder = "Celular"
-				campoInput.addEventListener("blur", () => {
-					const valor = campoInput.value.replace(/\D/g, "")
-					if (valor.length !== 11) {
-						campoInput.value = ""
-					}
-				})
-			}
-		}
-		const remover = document.createElement("button")
-		remover.classList.add("btn", "btn-danger", "remover")
-		remover.textContent = "X"
-		campo.appendChild(remover)
-		campos.appendChild(campo)
-		remover.addEventListener("click", () => {
-			campo.remove()
-		})
-	}
-})*/
