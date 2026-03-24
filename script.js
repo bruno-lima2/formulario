9c5a444306b0b7af43489032743daf6635dd55dc
Bruno Lima <bruuno.lima03@gmail.com>
2026-03-24 08:14:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,89 +1,46 @@
 const adicionar = document.querySelector(".adicionar")
 const selecionar = document.querySelector(".selecionar")
 const campos = document.querySelector(".campos")
+function criarCampo() {
+	const campo = document.createElement("div")
+	campo.classList.add("campo")
+	campos.appendChild(campo)
+	if (selecionar.value === "endereco") {
+		const campoTextarea = document.createElement("textarea")
+		campoTextarea.classList.add("form-control")
+		campoTextarea.placeholder = "Endereço"
+		campo.appendChild(campoTextarea)
+	} else {
+		const campoInput = document.createElement("input")
+		campoInput.classList.add("form-control")
+		if (selecionar.value === "email") {
+			campoInput.placeholder = "Email"
+		} else if (selecionar.value === "celular") {
+			campoInput.placeholder = "Celular"
+			validarCelular(campoInput)
+		}
+		campo.appendChild(campoInput)
+	}
+	criarBotao(campo)
+}
+function criarBotao(campo) {
+	const remover = document.createElement("button")
+	remover.textContent = "X"
+	remover.classList.add("btn", "btn-danger", "remover")
+	remover.addEventListener("click", () => {
+		campo.remove()
+	})
+	campo.appendChild(remover)
+}
+function validarCelular(campoInput) {
+	campoInput.addEventListener("blur", () => {
+		if (campoInput.value.replace(/\D/g, "").length !== 11) {
+		campoInput.value = ""
+		}
+	})
+}
 adicionar.addEventListener("click", () => {
 	if (selecionar.value) {
-		function criarCampo() {
-			const campo = document.createElement("div")
-			campo.classList.add("campo")
-			if (selecionar.value === "endereco") {
-				const campoTextarea = document.createElement("textarea")
-				campoTextarea.classList.add("form-control")
-				campoTextarea.placeholder = "Endereço"
-				campo.appendChild(campoTextarea)
-			} else {
-				const campoInput = document.createElement("input")
-				campoInput.type = "text"
-				campoInput.classList.add("form-control")
-				if (selecionar.value === "email") {
-					campoInput.placeholder = "Email"
-				} else {
-					campoInput.placeholder = "Celular"
-					campoInput.addEventListener("blur", () => {
-						if (campoInput.value.replace(/\D/g, "").length !== 11) {
-							campoInput.value = ""
-						}
-					})
-				}
-				campo.appendChild(campoInput)
-			}
-			campos.appendChild(campo)
-			function botaoRemover() {
-				const remover = document.createElement("button")
-				remover.textContent = "X"
-				remover.classList.add("btn", "btn-danger", "remover")
-				remover.addEventListener("click", () => {
-					campo.remove()
-				})
-				campo.appendChild(remover)
-			}
-			botaoRemover()
-		}
 		criarCampo()
 	}
 })
-
-/*const adicionar = document.querySelector(".adicionar")
-const selecionar = document.querySelector(".selecionar")
-const campos = document.querySelector(".campos")
-function criarCampo(tipo) {
-    const campo = document.createElement("div")
-    campo.classList.add("campo")
-    let input
-    if (tipo === "email" || tipo === "celular") {
-        input = document.createElement("input")
-        input.placeholder = tipo === "email" ? "Email" : "Celular"
-        input.classList.add("form-control")
-    } else if (tipo === "endereco") {
-        input = document.createElement("textarea")
-        input.placeholder = "Endereço"
-        input.classList.add("form-control")
-    }
-    campo.appendChild(input)
-    campo.appendChild(criarBotaoRemover(campo))
-    campos.appendChild(campo)
-    validarCampo(input, tipo)
-}
-function criarBotaoRemover(campo) {
-    const remover = document.createElement("button")
-    remover.textContent = "X"
-    remover.classList.add("btn", "btn-danger", "remover")
-    remover.addEventListener("click", () => {
-        campo.remove()
-    })
-    return remover
-}
-function validarCampo(input, tipo) {
-    if (tipo === "celular") {
-        input.addEventListener("blur", () => {
-            if (input.value.replace(/\D/g, "").length !== 11) {
-                input.value = ""
-            }
-        })
-    }
-}
-adicionar.addEventListener("click", () => {
-    if (selecionar.value) {
-        criarCampo(selecionar.value)
-    }
-})*/
