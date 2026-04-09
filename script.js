c8d67d2f5c1797c2aba00eab55e20a12126643c8
Bruno Lima <bruuno.lima03@gmail.com>
2026-04-09 08:12:20 -0300

    atualizar

--- script.js
+++ script.js
@@ -1,85 +1,90 @@
 const selecionar = document.querySelector(".selecionar");
 const adicionar = document.querySelector(".adicionar");
 const campos = document.querySelector(".campos");
-function campoEmail() {
+function criarCampo() {
   const campo = document.createElement("div");
   campo.classList.add("campo");
+  campos.appendChild(campo);
   const label = document.createElement("label");
   label.classList.add("label");
-  label.textContent = "Email";
+  campo.appendChild(label);
   const container = document.createElement("div");
   container.classList.add("container");
-  const campoEmail = document.createElement("input");
-  campoEmail.classList.add("form-control");
-  campoEmail.placeholder = "nome@dominio.com";
-  campos.appendChild(campo);
-  campo.appendChild(label);
   campo.appendChild(container);
-  container.appendChild(campoEmail);
-  botaoRemover(campo, container);
-  feedbackEmail(campoEmail, campo);
-}
-function botaoRemover(campo, container) {
+  const campoEntrada = document.createElement("input");
+  campoEntrada.classList.add("form-control");
+  container.appendChild(campoEntrada);
   const remover = document.createElement("button");
-  remover.classList.add("remover");
-  remover.textContent = "X";
   remover.classList.add("btn", "btn-danger", "remover");
+  remover.textContent = "X";
   container.appendChild(remover);
+  const feedback = document.createElement("div");
+  feedback.classList.add("invalid-feedback", "feedback");
+  if (selecionar.value === "email") {
+    campoEmail(label, campoEntrada, feedback);
+    validacaoEmail(campoEntrada, campo, feedback);
+  } else if (selecionar.value === "celular") {
+    campoCelular(label, campoEntrada, feedback);
+    validacaoCelular(campoEntrada, campo, feedback);
+    mascaraCelular(campoEntrada);
+  } else if (selecionar.value === "cep") {
+    campoCEP(label, campoEntrada, feedback);
+    validacaoCEP(campoEntrada, campo, feedback);
+    mascaraCEP(campoEntrada);
+  }
+  botaoRemover(remover, campo);
+}
+function campoEmail(label, campoEntrada, feedback) {
+  label.textContent = "Email";
+  campoEntrada.placeholder = "nome@dominio.com";
+  feedback.textContent = "O email é inválido";
+}
+function campoCelular(label, campoEntrada, feedback) {
+  label.textContent = "Celular";
+  campoEntrada.placeholder = "(00) 0 0000-0000";
+  feedback.textContent = "O número de celular é inválido";
+}
+function campoCEP(label, campoEntrada, feedback) {
+  label.textContent = "CEP";
+  campoEntrada.placeholder = "00000-000";
+  feedback.textContent = "O CEP é inválido";
+}
+function botaoRemover(remover, campo) {
   remover.addEventListener("click", () => {
     campo.remove();
   });
 }
-function feedbackEmail(campoEmail, campo) {
-  const feedback = document.createElement("div");
-  feedback.classList.add("invalid-feedback", "feedback");
-  feedback.textContent = "O email é inválido";
-  const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  campoEmail.addEventListener("blur", () => {
-    if (!regexEmail.test(campoEmail.value)) {
+function validacaoEmail(campoEntrada, campo, feedback) {
+  campoEntrada.addEventListener("blur", () => {
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!regexEmail.test(campoEntrada.value)) {
       campo.appendChild(feedback);
-      campoEmail.classList.add("is-invalid");
-    } else {
+    } else if (regexEmail.test(campoEntrada.value)) {
       feedback.remove();
-      campoEmail.classList.remove("is-invalid");
     }
   });
 }
-function campoCelular() {
-  const campo = document.createElement("div");
-  campo.classList.add("campo");
-  const label = document.createElement("label");
-  label.classList.add("label");
-  label.textContent = "Celular";
-  const container = document.createElement("div");
-  container.classList.add("container");
-  const campoCelular = document.createElement("input");
-  campoCelular.classList.add("form-control");
-  campoCelular.placeholder = "(00) 0 0000-0000";
-  campos.appendChild(campo);
-  campo.appendChild(label);
-  campo.appendChild(container);
-  container.appendChild(campoCelular);
-  botaoRemover(campo, container);
-  feedbackCelular(campoCelular, campo);
-  validacaoCelular(campoCelular);
-}
-function feedbackCelular(campoCelular, campo) {
-  const feedback = document.createElement("div");
-  feedback.classList.add("invalid-feedback", "feedback");
-  feedback.textContent = "O número de celular é inválido";
-  campoCelular.addEventListener("blur", () => {
-    if (campoCelular.value.replace(/\D/g, "").length !== 11) {
+function validacaoCelular(campoEntrada, campo, feedback) {
+  campoEntrada.addEventListener("blur", () => {
+    if (campoEntrada.value.replace(/\D/g, "").length !== 11) {
       campo.appendChild(feedback);
-      campoCelular.classList.add("is-invalid");
-    } else {
+    } else if (campoEntrada.value.replace(/\D/g, "").length === 11) {
       feedback.remove();
-      campoCelular.classList.remove("is-invalid");
     }
   });
 }
-function validacaoCelular(campoCelular) {
-  campoCelular.addEventListener("input", () => {
-    let valor = campoCelular.value.replace(/\D/g, "");
+function validacaoCEP(campoEntrada, campo, feedback) {
+  campoEntrada.addEventListener("blur", () => {
+    if (campoEntrada.value.replace(/\D/g, "").length !== 8) {
+      campo.appendChild(feedback);
+    } else if (campoEntrada.value.replace(/\D/g, "").length === 8) {
+      feedback.remove();
+    }
+  });
+}
+function mascaraCelular(campoEntrada) {
+  campoEntrada.addEventListener("input", () => {
+    let valor = campoEntrada.value.replace(/\D/g, "");
     let numeros = valor;
     if (valor.length > 0) {
       numeros = `(${valor.slice(0, 2)}`;
@@ -93,60 +98,21 @@ function validacaoCelular(campoCelular) {
     if (valor.length > 7) {
       numeros = `(${valor.slice(0, 2)}) ${valor.slice(2, 3)} ${valor.slice(3, 7)}-${valor.slice(7, 11)}`;
     }
-    campoCelular.value = numeros;
+    campoEntrada.value = numeros;
   });
 }
-function campoCEP() {
-  const campo = document.createElement("div");
-  campo.classList.add("campo");
-  const label = document.createElement("label");
-  label.classList.add("label");
-  label.textContent = "CEP";
-  const container = document.createElement("div");
-  container.classList.add("container");
-  const campoCEP = document.createElement("input");
-  campoCEP.classList.add("form-control");
-  campoCEP.placeholder = "00000-000";
-  campos.appendChild(campo);
-  campo.appendChild(label);
-  campo.appendChild(container);
-  container.appendChild(campoCEP);
-  botaoRemover(campo, container);
-  feedbackCEP(campoCEP, campo);
-  validacaoCEP(campoCEP);
-}
-function feedbackCEP(campoCEP, campo) {
-  const feedback = document.createElement("div");
-  feedback.classList.add("invalid-feedback", "feedback");
-  feedback.textContent = "O CEP é inválido";
-  campoCEP.addEventListener("blur", () => {
-    if (campoCEP.value.replace(/\D/g, "").length !== 8) {
-      campo.appendChild(feedback);
-      campoCEP.classList.add("is-invalid");
-    } else {
-      feedback.remove();
-      campoCEP.classList.remove("is-invalid");
-    }
-  });
-}
-function validacaoCEP(campoCEP) {
-  campoCEP.addEventListener("input", () => {
-    let valor = campoCEP.value.replace(/\D/g, "");
+function mascaraCEP(campoEntrada) {
+  campoEntrada.addEventListener("input", () => {
+    let valor = campoEntrada.value.replace(/\D/g, "");
     let numeros = valor;
     if (valor.length > 5) {
-      numeros = `${valor.slice(0, 5)}-`;
-    } if (valor.length > 5) {
       numeros = `${valor.slice(0, 5)}-${valor.slice(5, 8)}`;
     }
-    campoCEP.value = numeros;
+    campoEntrada.value = numeros;
   });
 }
 adicionar.addEventListener("click", () => {
-  if (selecionar.value === "email") {
-    campoEmail();
-  } else if (selecionar.value === "celular") {
-    campoCelular();
-  } else if (selecionar.value === "cep") {
-    campoCEP();
+  if (selecionar.value) {
+    criarCampo();
   }
 });
